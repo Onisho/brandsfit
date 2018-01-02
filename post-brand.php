198a7e13c09068504f5a1ca77dc23e7e457e0c8b
Jurgen Menu <yojurgen.menu@gmail.com>
2018-01-02 16:29:59 +0100

    google analytics + tag

--- post-brand.php
+++ post-brand.php
@@ -36,6 +36,14 @@ $result = mysql_query($sql) or die('Could not execute query.');
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Brandsfit - Brands.Set.Match.</title>
+  
+    <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MV2GVXH');</script>
+  <!-- End Google Tag Manager -->
     
   <?php
   switch($lang) {
@@ -50,6 +58,18 @@ $result = mysql_query($sql) or die('Could not execute query.');
 
 </head>
 
-<body></body>
+<body>
+  
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-5467127-49', 'auto');
+  ga('send', 'pageview');
+  </script>
+  
+</body>
 
 </html>
